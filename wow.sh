fb4a759ba8d1da3153fce1b2775aef126a4bb2de
Lam Hieu <lamhieu.vk@gmail.com>
2024-04-23 15:08:49 +0700

    Add scripts to run eval on notebook

--- wow.sh
+++ wow.sh
@@ -1,24 +1,27 @@
-# define prefixes base on codegeex-2 repo
-declare -A langs
-langs=( [py]="# Python" [js]="// JavaScript" [java]="// Java" [cpp]="// C++" [swift]="// Swift" [php]="// PHP" [jl]="# Julia" [lua]="// Lua" [r]="# R" [rkt]="; Racket" [rs]="// Rust" [d]="" )
+# after activating env and setting up accelerate...
+langs=(py js java cpp swift php d jl lua r rkt rs)
 
-for lang in "${!langs[@]}"; do
-    prefix="language: ${langs[$lang]}"
-    echo "For language $lang, the prefix is: $prefix"
+for lang in "${langs[@]}"; do
+    # use humaneval for py and multipl-e for the rest
+    if [ "$lang" == "py" ]; then
+        task=humaneval
+    else
+        task=multiple-$lang
+    fi
+
+    echo "Running task $task"
     generations_path=generations_$model/generations_$task\_$model.json
     accelerate launch main.py \
-            --model "$org/$model" \
-            --task "multiple-$lang" \
-            --n_samples 5 \
-            --batch_size 5 \
-            --limit 8 \
+            --model $org/$model \
+            --task $task \
+            --n_samples 50 \
+            --batch_size 50 \
             --max_length_generation 512 \
             --temperature 0.2 \
             --precision bf16 \
             --trust_remote_code \
             --use_auth_token \
             --generation_only \
-            --save_generations_path $generations_path \
-            --prefix \"$prefix\" \
+            --save_generations_path $generations_path
     echo "Task $task done"
 done
